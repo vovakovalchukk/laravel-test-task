2aa665470facbb68363a4a87cf78c5ae0b72f9b3
Uladzimir <uladzimir.kavalchuk@innowise-group.com>
2023-07-13 09:04:02 +0300

    add capacities

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Hotel extends Model
 {
     use HasFactory;
 
     protected $table = 'hotels';
+
+    public function capacities(): HasMany
+    {
+        return $this->hasMany(Capacity::class);
+    }
+
 }
---
+++ database/migrations/2023_07_11_094912_create_capacities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('capacities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('hotel_id');
+            $table->date('date');
+            $table->integer('capacity');
+            $table->timestamps();
+
+            $table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('capacities');
+    }
+};
