95f609d227b2cbaacef4f7560c4f3abfc5fc7fb8
Uladzimir <uladzimir.kavalchuk@innowise-group.com>
2023-07-13 15:47:41 +0300

    add saving statistic information in cache

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Services\Statistic\StatisticService;
+use Illuminate\Support\Facades\Cache;
 
 class StatisticController extends Controller
 {
+    public function __construct(
+        private readonly StatisticService $statisticService,
+    ) {}
+
     public function index(StatisticService $statisticService)
     {
-        $data = $statisticService->calculate();
+        $data = Cache::remember('statistic', 86400, function (){
+             return $this->statisticService->calculate();
+        });
 
         return view('statistic.dashboard', compact('data'));
     }
